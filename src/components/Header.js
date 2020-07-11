0f5ffc2024f9f0a813bb6ae77ba4a402230761c9
Ilya Karlyuk <divizionby0@gmail.com>
2020-07-11 08:07:04 +0300

    up96

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
 import React from "react";
-import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className={classes.header}>
+        <header className='header'>
             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT5cPz71IV38Ghvp1vU8Ljb69amTWiCr0aNywPR8WhkG9XvqWQy&usqp=CAU'/>
         </header>
     )
